0ec81dc00416b3e58340238d4d640b0b4591ef4f
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-09-24 00:03:24 +0300

    24.09

---
+++ Asp.NetCore5.0_MovieSiteProject/Controllers/AdminController.cs
@@ -0,0 +1,76 @@
+﻿using Asp.NetCore5._0_MovieSiteProject.Data;
+using Asp.NetCore5._0_MovieSiteProject.Entity;
+using Asp.NetCore5._0_MovieSiteProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_MovieSiteProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly Context _context;
+
+        public AdminController(Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MovieList()
+        {
+
+            return View(new AdminMoviesViewModel
+            {
+                Movies = _context.Movies.Include(m => m.Genres).ToList()
+
+            });
+        }
+        [HttpGet]
+        public IActionResult MovieUpdate(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var entity = _context.Movies.Select(m => new AdminEditViewModel
+            {
+
+                MovieId = m.MovieId,
+                Title = m.Title,
+                Description = m.Description,
+                ImageUrl = m.ImageUrl
+            }).FirstOrDefault(m => m.MovieId == id);
+
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            return View(entity);
+        }
+
+        [HttpPost]
+        public IActionResult MovieUpdate(AdminMovieViewModel model)
+        {
+            var entity = _context.Movies.Find(model.MovieId);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            entity.Title = model.Title;
+            entity.Description = model.Description;
+            entity.ImageUrl = model.ImageUrl;
+            _context.SaveChanges();
+            return RedirectToAction("MovieList");
+        }
+
+       
+   
+    }
+}
---
+++ Asp.NetCore5.0_MovieSiteProject/Models/AdminMoviesViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Asp.NetCore5._0_MovieSiteProject.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_MovieSiteProject.Models
+{
+    public class AdminMoviesViewModel
+    {
+        public List<Movie> Movies { get; set; }
+    }
+
+    public class AdminMovieViewModel
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public List<Genre> Genres { get; set; }
+    }
+    public class AdminEditViewModel
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string Description{ get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
