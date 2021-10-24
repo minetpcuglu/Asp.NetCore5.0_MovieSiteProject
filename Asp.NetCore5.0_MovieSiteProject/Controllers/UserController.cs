cd0c3c3f65bbd8da22f0c062fe76294721682cbc
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-10-25 00:00:44 +0300

    25.10 / UserController Validation Rule Add

---
+++ Asp.NetCore5.0_MovieSiteProject/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Asp.NetCore5._0_MovieSiteProject.Entity;
+using Asp.NetCore5._0_MovieSiteProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_MovieSiteProject.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser(UserModel user)
+        {
+            return View();
+        }
+    }
+}
---
+++ Asp.NetCore5.0_MovieSiteProject/Models/UserModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_MovieSiteProject.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        [Required]
+        [StringLength(20,ErrorMessage ="Max 20 karakter içermeli")]
+        public string    UserName { get; set; }
+        [Required]
+        [StringLength(15, ErrorMessage = "{0} Karakter uzunlugu {2}-{1} aralıgında olmalıdır",MinimumLength =3)]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress()]
+        public string Mail { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")] //karsılastırma
+        public string RePassword { get; set; }
+        
+        [Url]
+        public string Url { get; set; }
+    
+        [Range(1900,2021)]
+        public int BirthYear { get; set; }
+    }
+}
